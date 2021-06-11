aed1882e76a8fde9902f747e1447de6116aacb9d
Shall <81486074+mxliodas@users.noreply.github.com>
2021-06-11 20:15:45 +0200

    Event message

---
+++ events/message.js
@@ -0,0 +1,14 @@
+const config = require('../config.json')
+
+module.exports = async (client, message) =>
+
+	if (message.channel.type === 'dm') return
+	if (message.author.bot) return
+
+  const args = message.content.trim().split(/ +/g)
+  const commandName = args.shift().toLowerCase()
+  const command = client.commands.get(commandName.slice(prefix.length)) || client.commands.find(cmd => cmd.help?.aliases ? cmd.help?.aliases.includes(commandName.slice(prefix.length)) : null)
+  
+  if (!commandName.startsWith(prefix)) return
+  if (command) command.run(message, args, client, config, db)
+}
