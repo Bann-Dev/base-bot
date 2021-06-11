ff5b1ce73ad5bea455940741c54c24beecf97f25
Shall <81486074+mxliodas@users.noreply.github.com>
2021-06-11 20:22:19 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -5,10 +5,11 @@ module.exports = async (client, message) =>
 	if (message.channel.type === 'dm') return
 	if (message.author.bot) return
 
-  const args = message.content.trim().split(/ +/g)
-  const commandName = args.shift().toLowerCase()
-  const command = client.commands.get(commandName.slice(prefix.length))
+	const args = message.content.trim().split(/ +/g)
+	const commandName = args.shift().toLowerCase()
+	const command = client.commands.get(commandName.slice(prefix.length))
   
-  if (!commandName.startsWith(prefix)) return
-  if (command) command.run(message, args, client, config, db)
+	if (!commandName.startsWith(prefix)) return
+	if (command) command.run(message, args, client, config)
+
 }
